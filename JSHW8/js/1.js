78e7ce0c4782c095a736aa625109f519d3bdbb64
Bohdan Kletskyi <underwater3499@gmail.com>
2022-09-05 23:12:29 +0300

    f

---
+++ JSHW8/js/1.js
@@ -0,0 +1,12 @@
+setTimeout(() => {
+    let id = document.getElementById('optionsList');
+console.log(id,'render');
+}, 2000)
+
+let id = document.getElementById('optionsList');
+console.log(id,'render1');
+
+
+
+  
+
