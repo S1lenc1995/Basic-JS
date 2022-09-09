378c00389f3de3ffd2a6028e23f37812f18d001f
Bohdan Kletskyi <underwater3499@gmail.com>
2022-09-09 22:18:14 +0300

    d

---
+++ JSHW9/js/1.js
@@ -0,0 +1,24 @@
+
+
+let arr = ["Kharkiv", "Kiev", ["Borispol", "Irpin"], "Odessa", "Lviv", "Dnieper"];
+let parent = document.createElement('ul')
+
+
+
+/* let timer;
+let a = 3;
+obr();
+function obr(){
+    body.innerHTML = a;
+    a--;
+    if (a<0){
+        clearTimeout(timer);
+        parent.remove();
+        body.innerHTML = ""; // Другого не придумал )
+    }
+    else{
+        timer = setTimeout(obr, 1000);
+    } 
+   
+}
+   */
